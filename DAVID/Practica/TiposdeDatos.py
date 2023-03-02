3c28d018bf961077e54cb7a7cb85c9a49af99691
Romig17 <126369975+Romig17@users.noreply.github.com>
2023-03-02 14:44:11 -0600

    Actividad semana 6

---
+++ DAVID/Practica/TiposdeDatos.py
@@ -0,0 +1,27 @@
+#Tipos de datos enetros (int) y largo (long)
+numeroEntero = 9 
+print ("El valor de la variable numeroEntero es:",numeroEntero, "y su tipo de dato es", type (numeroEntero))
+numeroLargo = 9
+print ("El valor de la variable numeroLargo es:",numeroLargo, "y su tipo de dato es", type (numeroLargo))
+
+#Tipos de datos float
+numeroFlotante = 5.66
+print(numeroFlotante, type (numeroFlotante))
+
+#Tipo de datos complejos
+numeroComplejo = 5 + 5j
+print(numeroComplejo, type(numeroComplejo))
+
+#tipos de datos string
+nombre1 = "Aaron"
+nombre2 = "Geime"
+nombre3 = """Alicia 
+Estefany
+Guzman
+Lopez"""
+nombre4 = "Edgar Rebe \n Rivas Castro" 
+print (nombre1, type (nombre1))
+print (nombre2, type (nombre2))
+print (nombre3, type (nombre3))
+print (nombre4, type (nombre4))
+
